d0be2a3f48e30487dd044bd34197253ba4a9297e
t0ngk <39180519+t0ngk@users.noreply.github.com>
2023-10-02 23:24:57 +0700

    🎉 : Lab 08

---
+++ Lab08/ProductsService/src/test/java/com/t0ng/productsservice/ProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t0ng.productsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
